6986af5f43eea73ae6c1c8286be47820e24039a8
Den <39949334+shlvd@users.noreply.github.com>
2020-05-26 12:15:35 +0300

    fixed typo in results

--- polls/admin.py
+++ polls/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 from .models import Choice, Question
 
-ss ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.StackedInline):
     model = Choice
     extra = 3
 
@@ -14,6 +14,6 @@ class QuestionAdmin(admin.ModelAdmin):
     ]
     inlines = [ChoiceInline]
     list_display = ('question_text', 'pub_date', 'was_published_recently')
-    
+
 admin.site.register(Question, QuestionAdmin)
 #admin.site.register(Choice)
